ebc7345982b69516643011f68304e6ccca4e1025
Sihle Ndlovu <sihle@synthesis.co.za>
2025-12-17 22:22:55 +0200

    fix

--- acm.tf
+++ acm.tf
@@ -14,30 +14,11 @@ resource "aws_acm_certificate" "main" {
   }
 }
 
-# Cloudflare DNS records for ACM validation
-resource "cloudflare_record" "acm_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  }
-
-  zone_id = var.cloudflare_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  content = each.value.value
-  ttl     = 1
-  proxied = false
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# ACM Certificate Validation
+# ACM Certificate Validation (skip DNS record creation)
 resource "aws_acm_certificate_validation" "main" {
-  certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [for record in cloudflare_record.acm_validation : record.hostname]
+  certificate_arn = aws_acm_certificate.main.arn
+  
+  timeouts {
+    create = "10m"
+  }
 }
